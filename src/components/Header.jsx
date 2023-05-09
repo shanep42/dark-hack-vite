94b951b39ec0cd3f4a5c0727944113759bea34cf
Shane <shanep42@gmail.com>
2023-05-08 22:58:52 -0500

    Adds a mere single font because I was playing with chakra

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,12 @@ const Header = () => {
 
     return (
         <>
-            <Heading color='white' marginBottom='1rem'>
+            <Heading color='white' marginBottom='1rem' font-family='Vesper Libre'>
                 DARK HACK RPG
             </Heading>
+            <Text fontFamily='Vesper Libre'>
+                Dark Hack is so called because it's an attempt to carry the design principles of Gray Hack into a fantasy game using the Forged in the Dark rules system
+            </Text>
         </>
     )
 }
